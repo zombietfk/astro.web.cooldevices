9d7a2467af919da44c28127c67e045a84faf7d89
Red Dead Lion <sam.m.w.judge+red.dead.lion@googlemail.com>
2026-01-10 11:04:42 -0600

    Added first article!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-  site: "https://zombietfk.github.io"
+  site: "https://zombietfk.github.io",
+  integrations: [mdx()]
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -5,11 +5,11 @@ const contentCollection = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string().optional(),
-        pubDate: z.string().optional(),
-        heroImage: z.string().optional(),
-        preview: z.string().optional(),
+        pubDate: z.date().optional(),
+        tags: z.array(z.string()).optional(),
+        keywords: z.array(z.string()).optional(),
     }),
-}),
+});
 
 export const collections = {
     other: contentCollection,
