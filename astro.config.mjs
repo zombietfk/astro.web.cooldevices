b42bd3371b7bc9fde5e403ab6dc2f5a046ea974e
Red Dead Lion <sam.m.w.judge+red.dead.lion@googlemail.com>
2026-01-11 14:18:07 -0600

    Added robots.txt, sitemap-index.xml, removed google auth file (now auths via DNS)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://zombietfk.github.io",
-  integrations: [mdx()]
+  site: "https://www.cooldevices.online",
+  integrations: [mdx(), sitemap()]
 });
